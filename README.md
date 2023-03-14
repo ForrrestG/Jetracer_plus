c9def92478c6f35a1c258bf7312443d0dee27c43
ForrrestG <125993390+ForrrestG@users.noreply.github.com>
2023-03-14 08:16:50 -0600

    pathfinder_vehichel_contral
    changed read.me file and added command code

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# jetracer_plus
-code for a better retracer written by FG 
+#jetracer_plus
+code for a better jetracer written by FG 
+#Forrest Gaarcia
+lets make the jetracer go off a ramp
+
+
---
+++ pathfinder_vehichle_contral_NC.py
@@ -0,0 +1,11 @@
+
+#pathfinders_vehichle_contral_NC.py
+
+#importing required libraries
+from jetracer.nvidia_racecar Import NvidiaRacecar
+
+# Instantiate the object for Object Oriented Programming (OOP)
+car = NvidiaRacecar()
+
+# Our first command of the vehicle
+car.steering = 0.3
