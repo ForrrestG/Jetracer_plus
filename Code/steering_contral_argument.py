fa5ae97423d923f496dce3a7516efe7a9ae7fba5
ForrrestG <125993390+ForrrestG@users.noreply.github.com>
2023-04-18 07:52:39 -0600

    with comments
    added comments and fixed a bug

--- Code/steering_contral_argument.py
+++ Code/steering_contral_argument.py
@@ -1,15 +1,16 @@
-from jetracer.nvidia_racercar import NvidarRacecar
+#Importing required libraies
+from jetracer.nvidia_racecar import NvidaRacecar
 
 import argparse 
-
+#createing the objects, for control over the car robot and parser
 car = NvidiaRacecar()
 
 parser = argparce.ArgumentParser()
-
+#adding a way to input into the program "--value" flag
 parser.add_argument('--value', type=float, required=True)
-
+#we are parcing the input of the usser
 args = parser.parse_args()
-
+#we're commanding the car to change steering
 car.steering = args.value
 
 print=("the command servor value is:  ", args.value)
